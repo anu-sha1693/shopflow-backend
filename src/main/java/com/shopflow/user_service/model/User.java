1e55b3a420fcfff77c2f671c37cdd2cf3ec4317d
anu-sha1693 <anushasunkara94@gmail.com>
2026-07-18 09:48:08 +0530

    Add userRepo

--- src/main/java/com/shopflow/user_service/model/User.java
+++ src/main/java/com/shopflow/user_service/model/User.java
@@ -7,11 +7,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
-public class Users {
+@Table(name = "users")
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
