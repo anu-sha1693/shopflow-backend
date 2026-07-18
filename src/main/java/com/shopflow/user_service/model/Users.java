62ca092107c725af506f5c84da218f2773dfa9b3
anu-sha1693 <anushasunkara94@gmail.com>
2026-07-18 09:11:40 +0530

    feat: add User entity with JPA mapping

---
+++ src/main/java/com/shopflow/user_service/model/Users.java
@@ -0,0 +1,48 @@
+package com.shopflow.user_service.model;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import lombok.Data;
+
+@Data
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
+    private String address;
+
+    private String role;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+    }
+
+
+}
